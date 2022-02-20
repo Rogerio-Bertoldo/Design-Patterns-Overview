3ab7afac1aafdafd14f3342da8e6bfcd973da48a
Rogerio Bertoldo <rogeriohenriquehbt@gmail.com>
2022-02-20 00:12:14 -0300

    Cleaning code

--- observer/main.py
+++ observer/main.py
@@ -4,10 +4,12 @@ from events import Event
 
 if __name__ == '__main__':
     job_notifier = JobNotifier()
-    dev = Candidate(name="Rogerio", job="Senior Software developer", email="jayimo5237@ishop2k.com")
-    po = Candidate(name="Desiree", job="Product Owner", email="jafyadulmi@vusra.com")
+    
+    dev = Candidate(name="Rogerio", job="Software developer", email="emailexample@domainexample.com")
+    po = Candidate(name="Bob", job="Manager", email="emailexample@domainexample.com")
 
     dev.setAlert(job_notifier, Event(dev.job))
     po.setAlert(job_notifier, Event(po.job))
-    job_notifier.addNewJob("Senior Software developer")
+
+    job_notifier.addNewJob("Software developer")
     print('Code is not blocked :)')
